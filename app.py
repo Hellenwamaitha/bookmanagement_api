f628c828326dc6acf73a4a249cb8f89aba1017f9
Hellen Wamaitha <hellenwama254@gmail.com>
2024-08-01 13:19:20 +0300

    create the app file

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import flask, jsonfiy,request
+
+app = flask(_name_)
+
+#sample data
+books = [
+    {'id': 1, 'title': '1984', 'author': 'George Orwell'},
+    {'id': 2, 'title': 'To Kill a Mockingbird', 'author': 'Harper Lee'},
+]
